c9de426da2ace24b6ccf13a9b6b1d418eaed358c
john <john.derr@gmail.com>
2016-06-24 09:45:03 -0400

    config changes

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "afternoon-springs-44678", port: 443],
+  url: [host: "afternoon-springs-44678.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
   #cache_static_manifest: "priv/static/manifest.json"
 
